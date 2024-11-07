307132dbca1f8c04e8aa13e954cc0572d9f2a233
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-11-07 22:23:05 +0500

    Events(keydown,keyup,blur etc) in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConnectableObservable, last } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'newLatestAppModule';
+  getName() {
+    alert("Function called!");
+  }
+  getName_1(name: any, lastName: any) {
+    alert(name + lastName);
+  }
+  getData(val: any) {
+    console.log(val);
+  }
 }
